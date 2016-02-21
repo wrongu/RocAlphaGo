44fbe293a945215a370839cedaeb00f3d8c2c8a4
yuewang0319 <yuewang@dhcp-10-5-18-57.wireless.rochester.edu>
2016-02-21 18:52:58 -0500

    MCTS Tree Search Class updated

--- AlphaGo/mcts.py
+++ AlphaGo/mcts.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import numpy as np
 import policy
 import value
@@ -5,47 +6,37 @@ import shallow_policy
 import random
 import go
 
-SIMULATIONS = 100
+
 LAMBDA = 0.5
-L = 20
+
+
 
 class TreeNode(object):
          
         def __init__(self):
+                
                 self.nVisits = 0
-                self.toValue = 0
+                self.Q_value = 0
+                self.u_value = 0
                 self.children = []  
 
-
-        def DFS(self, Depth):
-                # need to move this part to MCTS class
-                visited = [] 
-                visited.insert(0,self)
-                for n in range(0,Depth):
-                    self.expansion(nActions)
-                    self = self.selection()
-                    visited.insert(0,self)
-                for treenode in visited:
-                    treenode.updateVisits()
-                    if(treenode.isLeaf()==False):
-                        treenode.backup()
-                    else:
-                        
 
         def selection(self):
                 # select among children nodes with maximum value
                 maxValue = 0
-                selected = TreeNode()
-                for treenode in self.children:
-                    if(treenode.toValue > maxValue):
-                        selected = treenode
-                        maxValue = treenode.toValue
-                return selected
+                action = (0,0)
+                selectednode = TreeNode()
+                for child in self.children:
+                    if(child[1].toValue() > maxValue):
+                        selectednode = child[1]
+                        maxValue = child[1].toValue()
+                        action = child[0]
+                return selectednode, action
 
-        def expansion(self, nActions):
+        def expansion(self, probs):
                 # expand children nodes
-                for i in range(0, nActions): 
-                    self.children.append(TreeNode())
+                for prob in probs: 
+                    self.children.append((prob[0],TreeNode()))
                     
         def isLeaf(self):
                 # check if reaches leaf state
@@ -55,22 +46,110 @@ class TreeNode(object):
                 # update the number of visits
                 self.nVisits += 1
 
-        def updateValues(self, value):
-                # update values 
-                self.toValue += value
+        def updateLeafStats(self, value):
+            
+                # update Q value and counts of visits for leaf state
+                self.Q_value = self.Q_value * self.nVisits + value
+                self.nVisits += 1
+                self.Q_value = self.Q_value / self.nVisits
+        
+        def updateBonus(self, probs):
+                
+                # update u value from a list of prior probability from policy function
+                for index in range(0, len(self.children)):  
+                    self.children[index].u_value = probs[index][1] / (1 + self.children[index].nVisits)
 
-        def backup(self):
+        def backUp(self):
                 # Backpropagate values by averaging values of subtree 
                 sumValue = 0
                 for treenode in self.children:
-                    sumValue += treenode.toValue
+                    sumValue += treenode.Q_value
                 self.toValue = sumValue/len(self.children)
                 
-                
+        def toValue(self):
+
+                # evaluate value of treenode from both Q value and u value
+                return self.Q_value + self.u_value
+
+
 
 class MCTS(object):
-    pass
     
+        def __init__(self, state):        
+                
+                self.state = state
+                self.treenode = TreeNode()
+                
+
+        def DFS(self, nDepth, traversed):
+                # Depth First Search Tree Traverse from start state over certain depths, keep track and update statistics for all of the traversed edges: (state, action, treenode)pair
+                visited = []
+                treenode = self.treenode
+                state = self.state
+
+                for index in range(0, nDepth):
+                        
+                    probs=priorProb(state)
+                    treenode.expansion(probs)
+                    treenode.updateBonus(probs)
+                    treenode, action = treenode.selection() 
+                    # need do_move(action) function to return updated state
+                    state = state.do_move(action) 
+                    visited.insert(0, (state, action, treenode)) 
+                
+                for index in range(0, nDepth):
+                    
+                    if(visited[index][2].isLeaf() == False):
+                        for item in traversed[index]:
+                            #need function from GameState class to check if two state equals
+                            if(item[0].equalstate(visited[index][0]):
+                                item[2].updateVisits()
+                                item[2].backUp()
+                        
+                        visited[index][2].updatedVisits()
+                        visited[index][2].backUp()
+                        traversed[index].append(visited[index])
+                    else:
+                        value = self.leaf_evaluation(visited[index][0])
+                        for item in traversed[index]:
+                            #need function from GameState class to check if two state equals
+                            if(item[0].equalstate(visited[index][0]):
+                                item[2].updateLeafStats(value)
+
+                        visited[index][2].updateLeafStats(value)
+                        traversed[index].append(visited[index])
+                    
+                        
+        def leafEvaluation(self, state):
+                # return weighted average between ramdom rollout by fast policy and value function 
+                z = somerolloutfunction(state)
+                v = somevaluefunction(state)
+                return (1-LAMBDA)*v+LAMBDA*z  
+        
+        def priorProb(self, state):
+                
+                # return a list of action, prior probality of from policy function of a given state
+                probs = somepolicyfunction(state)
+                return probs
+
+        def getMove(self, nSimulations, nDepth = 20):
+               
+                # run MSTC simulations for a number of times and return the best move 
+                traversed = []
+
+                for index in range(0, nDepth):
+                    sublist = []
+                    traversed.append(sublist)
+
+                for n in range(0, nSimulations):
+                    self.DFS(nDepth, traversed)
+
+                treenode, action = self.treenode.selection() 
+                return action
+
+
+
+
 
 class ParallelMCTS(MCTS):
-	pass
+        pass
