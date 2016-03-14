7b5e3030a926d5c0f6362aeba98b8e781ff234d6
yuewang0319 <yuewang@dhcp-10-5-3-207.wireless.rochester.edu>
2016-03-14 16:48:36 -0400

    use get_legal_moves to append legal actions for testing purposes

--- tests/test_mcts.py
+++ tests/test_mcts.py
@@ -10,7 +10,7 @@ class TestMCTS(unittest.TestCase):
     
     def setUp(self):
         self.s = GameState()
-        self.mcts = MCTS(s, value_network, policy_network, rollout_policy)
+        self.mcts = MCTS(self.s, value_network, policy_network, rollout_policy)
         self.treenode = TreeNode()
 
 
@@ -19,24 +19,29 @@ class TestMCTS(unittest.TestCase):
         self.treenode.expansion(actions)
         self.treenode.updateU_value(actions)
         selectednode, selectedaction = self.treenode.selection()
-        self.assertEqual(max(actions, key = lambda x:x[1]), selectednode.toValue(), 'incorrect node selected')
-        self.assertEqual(max(actions, key = lambda x:x[0]), selectedaction, 'incorrect action selected')
+        self.assertEqual(max(actions, key = lambda x:x[1])[1], selectednode.toValue(), 'incorrect node selected')
+        self.assertEqual(max(actions, key = lambda x:x[1])[0], selectedaction, 'incorrect action selected')
     
     def test_mcts_DFS(self):
-        value = self.mcts.leafEvaluation(self.s)
         treenode = self.mcts.DFS(3, self.treenode, self.s) 
         self.assertEqual(1, treenode.nVisits, 'incorrect visit count')
-        self.assertEqual(value/2*9, treenode.Q_value, 'incorrect Q_value') 
+ 
 
     def test_mcts_getMove(self):
-        action = self.mcts.getMove(2, 3)
+        action = self.mcts.getMove(3, 10)
         self.assertIsNotNone(action,'no output action') 
-        print("getMove checked")
+        action = self.mcts.getMove(4, 5)
+        self.assertIsNotNone(action,'no output action') 
+        action = self.mcts.getMove(6, 8)
+        self.assertIsNotNone(action,'no output action') 
+
 
 def policy_network(state):
+    s = GameState()
+    moves = s.get_legal_moves()
     actions = []
-    for i in range(0, 10):
-        actions.append(((i, i+1), random.uniform(0, 1)))                              
+    for move in moves:
+        actions.append((move, random.uniform(0, 1)))                              
     return actions 
    
 def value_network(state):
