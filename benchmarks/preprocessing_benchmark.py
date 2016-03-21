44d0b03caee15b52c361af4e656c2a3c56856a4e
wrongu <rlange@ur.rochester.edu>
2016-03-21 11:57:11 -0400

    fixed preprocessing_benchmark for new data and game_converter structure

--- benchmarks/preprocessing_benchmark.py
+++ benchmarks/preprocessing_benchmark.py
@@ -5,7 +5,11 @@ prof = Profile()
 
 test_features = ["board", "turns_since", "liberties", "capture_size", "self_atari_size", "liberties_after", "sensibleness", "zeros"]
 gc = game_converter()
-args = ('tests/test_sgfs/AlphaGo-vs-Lee-Sedol-20160310-first10only.sgf', test_features)
+args = ('tests/test_data/sgf/Lee-Sedol-vs-AlphaGo-20160309.sgf', test_features)
 
-prof.runcall(gc.convert_game, *args)
+def run_convert_game():
+	for traindata in gc.convert_game(*args):
+		pass
+
+prof.runcall(run_convert_game)
 prof.dump_stats('bench_results.prof')
