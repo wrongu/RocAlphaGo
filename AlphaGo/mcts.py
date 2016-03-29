545486361b269311cbfb3678d3213a575e9f5b50
wrongu <rlange@ur.rochester.edu>
2016-03-29 15:55:11 -0400

    whitespace cleanup (passing flake8)

--- AlphaGo/mcts.py
+++ AlphaGo/mcts.py
@@ -2,17 +2,14 @@
 LAMBDA = 0.5
 
 
-
 class TreeNode(object):
 	"""Tree Representation of MCTS that covers Selection, Expansion, Evaluation, Backup
 	"""
 	def __init__(self):
-				
 		self.nVisits = 0
 		self.Q_value = 0
 		self.u_value = 0
-		self.children = {} 
-
+		self.children = {}
 
 	def expansion(self, actions):
 		"""Expand subtree --a dictionary with a tuple of (x,y) position as keys, TreeNode object as values
@@ -24,34 +21,32 @@ class TreeNode(object):
 		None
 
 		"""
-		for action in actions: 
+		for action in actions:
 			self.children[action[0]] = TreeNode()
 
 	def selection(self):
 		"""Select among subtree to get the position that gives maximum action value Q plus bonus u(P)
 
 		Keyword arguments:
-		None. 
+		None.
 
 		Return:
 		action -- a tuple of (x, y)
 		treenode object
-		
+
 
 		"""
 		selectednode = self.children.values()[0]
 		selectedaction = self.children.keys()[0]
 		maxValue = selectednode.toValue()
-				
+
 		for child in self.children.items():
-			if(child[1].toValue() > maxValue):
+			if child[1].toValue() > maxValue:
 				selectednode = child[1]
 				maxValue = child[1].toValue()
 				selectedaction = child[0]
 		return selectednode, selectedaction
 
-
-					
 	def isLeaf(self):
 		"""Check if leaf state is reached
 		"""
@@ -59,16 +54,16 @@ class TreeNode(object):
 		return self.children == {}
 
 	def updateVisits(self):
-		"""Update the count of visit times 
+		"""Update the count of visit times
 		"""
 		self.nVisits += 1
 
 	def updateQ_value(self, value):
-		"""Update the action value Q 
+		"""Update the action value Q
 		"""
 		self.Q_value = (self.Q_value * self.nVisits + value) / (self.nVisits + 1)
-			
-	def updateU_value(self, actions): 
+
+	def updateU_value(self, actions):
 
 		"""Update the bonus value u(P)--proportional to the prior probability but decays with the number of visits to encourage exploration
 
@@ -80,7 +75,7 @@ class TreeNode(object):
 
 		"""
 
-		for index in range(0, len(self.children)):  
+		for index in range(0, len(self.children)):
 			self.children[actions[index][0]].u_value = actions[index][1] / (1 + self.children[actions[index][0]].nVisits)
 
 	def backUp(self, value):
@@ -96,54 +91,54 @@ class TreeNode(object):
 		"""
 		return value / len(self.children)
 
-				
 	def toValue(self):
 		"""Return action value Q plus bonus u(P)
 		"""
 		return self.Q_value + self.u_value
 
+
 class MCTS(object):
 	"""Monte Carlo tree search, takes an input of game state, value network function, policy network function, rollout policy function, outputs an action after lookahead search is complete.
 	"""
 
-	def __init__(self, state, value_network, policy_network, rollout_policy):        
-				
+	def __init__(self, state, value_network, policy_network, rollout_policy):
+
 		self.state = state
 		self.treenode = TreeNode()
 		self._value = value_network
 		self._policy = policy_network
 		self._rollout = rollout_policy
-				
+
 	def DFS(self, nDepth, treenode, state):
-		"""Monte Carlo tree search over a certain depth per simulation, at the end of simulation, 
+		"""Monte Carlo tree search over a certain depth per simulation, at the end of simulation,
 		the action values and visits of counts of traversed treenode are updated.
 
 		Keyword arguments:
 		Initial GameState object
-		Initial TreeNode object 
+		Initial TreeNode object
 		Search Depth
 
 		Return:
 		TreeNode object with updated statistics(visit count N, action value Q)
 		"""
-				
+
 		visited = []
 		visited.insert(0, (state, treenode))
-				
-		for index in range(0, nDepth-1):      
+
+		for index in range(0, nDepth - 1):
 			actions = self.priorProb(state)
 			treenode.expansion(actions)
 			treenode.updateU_value(actions)
-			treenode, action = treenode.selection() 
+			treenode, action = treenode.selection()
 			state.do_move(action)
-			visited.insert(0, (state, treenode)) 
-				
-		for index in range(0, len(visited)): 
-			if(visited[index][1].isLeaf() == True):
+			visited.insert(0, (state, treenode))
+
+		for index in range(0, len(visited)):
+			if visited[index][1].isLeaf():
 				value = self.leafEvaluation(visited[index][0])
-			else:    
+			else:
 				value = visited[index][1].backUp(value)
-		
+
 		visited[-1][1].updateQ_value(value)
 		visited[-1][1].updateVisits()
 		return visited[-1][1]
@@ -159,9 +154,9 @@ class MCTS(object):
 		value
 		"""
 		z = self._rollout(state)
-		v = self._value(state) 
-		return (1-LAMBDA) * v + LAMBDA * z  
-		
+		v = self._value(state)
+		return (1 - LAMBDA) * v + LAMBDA * z
+
 	def priorProb(self, state):
 		"""Get a list of (action, probability) pairs according to policy network function outputs
 
@@ -170,16 +165,15 @@ class MCTS(object):
 
 		Return:
 		list of tuples ((x,y), probability)
-		
+
 		"""
 		actions = self._policy(state)
 
-		return actions   
-	   
+		return actions
 
 	def getMove(self, nDepth, nSimulations):
-				
-		"""After running simulations for a certain number of times, when the search is complete, an action is selected 
+
+		"""After running simulations for a certain number of times, when the search is complete, an action is selected
 		from root state
 
 		Keyword arguments:
@@ -192,8 +186,8 @@ class MCTS(object):
 		actions = self.priorProb(self.state)
 		self.treenode.expansion(actions)
 
-		for n in range(0, nSimulations):                    
-				
+		for n in range(0, nSimulations):
+
 			self.treenode.updateU_value(actions)
 			treenode, action = self.treenode.selection()
 			state = self.state.copy()
@@ -201,14 +195,10 @@ class MCTS(object):
 			treenode = self.DFS(nDepth, treenode, state)
 			self.treenode.children[action] = treenode
 
-		self.treenode.updateU_value(actions)   
+		self.treenode.updateU_value(actions)
 		treenode, action = self.treenode.selection()
 		return action
 
+
 class ParallelMCTS(MCTS):
-	  pass
-
-
-
-
-
+	pass
--- tests/test_mcts.py
+++ tests/test_mcts.py
@@ -2,57 +2,53 @@ from AlphaGo.go import GameState
 from AlphaGo.mcts import MCTS
 from AlphaGo.mcts import TreeNode
 import random
-import numpy as np
 import unittest
 
 
 class TestMCTS(unittest.TestCase):
-    
-    def setUp(self):
-        self.s = GameState()
-        self.mcts = MCTS(self.s, value_network, policy_network, rollout_policy)
-        self.treenode = TreeNode()
 
+	def setUp(self):
+		self.s = GameState()
+		self.mcts = MCTS(self.s, value_network, policy_network, rollout_policy)
+		self.treenode = TreeNode()
 
-    def test_treenode_selection(self):
-        actions = self.mcts.priorProb(self.s)
-        self.treenode.expansion(actions)
-        self.treenode.updateU_value(actions)
-        selectednode, selectedaction = self.treenode.selection()
-        self.assertEqual(max(actions, key = lambda x:x[1])[1], selectednode.toValue(), 'incorrect node selected')
-        self.assertEqual(max(actions, key = lambda x:x[1])[0], selectedaction, 'incorrect action selected')
-    
-    def test_mcts_DFS(self):
-        treenode = self.mcts.DFS(3, self.treenode, self.s) 
-        self.assertEqual(1, treenode.nVisits, 'incorrect visit count')
- 
+	def test_treenode_selection(self):
+		actions = self.mcts.priorProb(self.s)
+		self.treenode.expansion(actions)
+		self.treenode.updateU_value(actions)
+		selectednode, selectedaction = self.treenode.selection()
+		self.assertEqual(max(actions, key=lambda x: x[1])[1], selectednode.toValue(), 'incorrect node selected')
+		self.assertEqual(max(actions, key=lambda x: x[1])[0], selectedaction, 'incorrect action selected')
 
-    def test_mcts_getMove(self):
-        action = self.mcts.getMove(3, 10)
-        self.assertIsNotNone(action,'no output action') 
-        action = self.mcts.getMove(4, 5)
-        self.assertIsNotNone(action,'no output action') 
-        action = self.mcts.getMove(6, 8)
-        self.assertIsNotNone(action,'no output action') 
+	def test_mcts_DFS(self):
+		treenode = self.mcts.DFS(3, self.treenode, self.s)
+		self.assertEqual(1, treenode.nVisits, 'incorrect visit count')
+
+	def test_mcts_getMove(self):
+		action = self.mcts.getMove(3, 10)
+		self.assertIsNotNone(action, 'no output action')
+		action = self.mcts.getMove(4, 5)
+		self.assertIsNotNone(action, 'no output action')
+		action = self.mcts.getMove(6, 8)
+		self.assertIsNotNone(action, 'no output action')
 
 
 def policy_network(state):
-    s = GameState()
-    moves = s.get_legal_moves()
-    actions = []
-    for move in moves:
-        actions.append((move, random.uniform(0, 1)))                              
-    return actions 
-   
-def value_network(state):
+	s = GameState()
+	moves = s.get_legal_moves()
+	actions = []
+	for move in moves:
+		actions.append((move, random.uniform(0, 1)))
+	return actions
 
-    return 0.5
+
+def value_network(state):
+	return 0.5
+
 
 def rollout_policy(state):
+	return 1
 
-    return 1
 
 if __name__ == '__main__':
-    unittest.main()
-
-
+	unittest.main()
