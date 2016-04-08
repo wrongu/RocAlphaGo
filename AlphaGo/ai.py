b3c3c72a7b09ed9abc0ca1d5b22dcb8fe1a4d61b
wrongu <rlange@ur.rochester.edu>
2016-04-08 11:51:09 -0400

    shortened GreedyPolicyPlayer, added SamplingPolicyPlayer

--- AlphaGo/ai.py
+++ AlphaGo/ai.py
@@ -1,21 +1,48 @@
+import numpy as np
+from AlphaGo import go
+
+
 class GreedyPolicyPlayer(object):
-	"""A CNN player that uses a greedy policy (i.e. chooses the highest probability
-	   move at each point)
+	"""A player that uses a greedy policy (i.e. chooses the highest probability
+	move each turn)
 	"""
+
 	def __init__(self, policy_function):
 		self.policy = policy_function
 
 	def get_move(self, state):
-		action_probs = self.policy.eval_state(state)
-		if len(action_probs) > 0:
-			sensible_actions = [a for a in action_probs if not state.is_eye(
-				a[0], state.current_player)]
-			if len(sensible_actions) > 0:
-				max_prob = max(sensible_actions, key=lambda (a, p): p)
-				return max_prob[0]
-			else:
-				# No legal moves available, do so pass move
-				return None
-		else:
-			# No legal moves available, do so pass move
-			return None
+		sensible_moves = [move for move in state.get_legal_moves() if not state.is_eye(move, state.current_player)]
+		if len(sensible_moves) > 0:
+			move_probs = self.policy.eval_state(state, sensible_moves)
+			max_prob = max(move_probs, key=lambda (a, p): p)
+			return max_prob[0]
+		# No 'sensible' moves available, so do pass move
+		return go.PASS_MOVE
+
+
+class SamplingPolicyPlayer(object):
+	"""A player that samples a move in proportion to the probability given by the
+	policy.
+
+	By manipulating the 'temperature', moves can be pushed towards totally random
+	(high temperature) or towards greedy play (low temperature)
+	"""
+
+	def __init__(self, policy_function, temperature=1.0):
+		assert(temperature > 0.0)
+		self.policy = policy_function
+		self.exp = 1.0 / temperature
+
+	def get_move(self, state):
+		sensible_moves = [move for move in state.get_legal_moves() if not state.is_eye(move, state.current_player)]
+		if len(sensible_moves) > 0:
+			move_probs = self.policy.eval_state(state, sensible_moves)
+			# zip(*list) is like the 'transpose' of zip; zip(*zip([1,2,3], [4,5,6])) is [(1,2,3), (4,5,6)]
+			moves, probabilities = zip(*move_probs)
+			probabilities = np.array(probabilities)
+			probabilities = probabilities ** self.exp
+			probabilities = probabilities / probabilities.sum()
+			# numpy interprets a list of tuples as 2D, so we must choose an _index_ of moves then apply it in 2 steps
+			choice_idx = np.random.choice(range(len(moves)), p=probabilities)
+			return moves[choice_idx]
+		return go.PASS_MOVE
