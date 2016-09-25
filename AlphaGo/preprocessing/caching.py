00db654136f9505008fa13d581551253c3a98fd2
wrongu <rlange@ur.rochester.edu>
2016-09-25 09:37:13 -0400

    Added generic LRUCache decorator and associated tests

---
+++ AlphaGo/preprocessing/caching.py
@@ -0,0 +1,109 @@
+class DLLNode(object):
+    '''A single node in a doubly-linked list.
+    '''
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = self
+        self.prev = self
+
+    def insert_after(self, other):
+        self.next.prev = other
+        other.next = self.next
+        self.next = other
+        other.prev = self
+
+    def insert_before(self, other):
+        self.prev.next = other
+        other.prev = self.prev
+        self.prev = other
+        other.next = self
+
+    def splice(self):
+        self.prev.next = self.next
+        self.next.prev = self.prev
+        self.next = self
+        self.prev = self
+
+
+class LRUCache(object):
+    '''A least-recently used cache. This is a key-value store with a limit on the number of
+    elements that can be stored. When elements are added that would make the size of the cache
+    larger than its limit, the least-recently used key/value pair is deleted.
+
+    Both writing (using put) and reading (using get) count as 'using' a key/value pair.
+
+    The implementation uses a doubly-linked list to keep track of the order in which elements were
+    accessed. self.sentinel.next is always the most-recently-used, and self.sentinel.prev is always
+    the least-recently used. self.lookup is a dictionary mapping from keys to DLL nodes. Between
+    hash table lookups and DLL operations, all updates are done in constant-time (amortized).
+    '''
+
+    def __init__(self, max_size=1e5):
+        self.sentinel = DLLNode(None, None)
+        self.lookup = {}
+        self.max_size = max_size
+        self.size = 0
+
+    def get(self, key):
+        '''If 'key' is in the cache, return a tuple (True, value). Otherwise, return (False, None).
+        '''
+        if key in self.lookup:
+            self._refresh(key)
+            return True, self.lookup[key].value
+        else:
+            return False, None
+
+    def _refresh(self, key):
+        '''Move keyed item to the front of the DLL. key must be in the cache.
+        '''
+        node = self.lookup[key]
+        node.splice()
+        self.sentinel.insert_after(node)
+
+    def put(self, key, value):
+        '''Put value into cache or refresh existing value. If the cache size exceeds max_size, the
+        least recently used key/value pair is removed.
+        '''
+        if key in self.lookup:
+            self._refresh(key)
+            self.lookup[key].value = value
+        else:
+            # Add this key/value pair to the cache.
+            node = DLLNode(key, value)
+            self.sentinel.insert_after(node)
+            self.lookup[key] = node
+            # Keep size below max_size.
+            if self.size == self.max_size:
+                # Delete least recently used key/value pair.
+                lru_key = self.sentinel.prev.key
+                self.delete(lru_key)
+            self.size += 1
+
+    def delete(self, key):
+        '''Remove an item from the cache. No error is raised if key is not in the cache.
+        '''
+        if key in self.lookup:
+            self.lookup[key].splice()
+            del self.lookup[key]
+            self.size -= 1
+
+
+def lru_cache(max_size, key_fn=None):
+    '''Decorator that wraps a function in an LRU cache. If no key_fn is given, the function's args
+    are used as the cache key (they must be hashable).
+    '''
+
+    cache = LRUCache(max_size=max_size)
+
+    def lru_decorator(fn):
+        def lookup_or_execute(*args, **kwargs):
+            key = tuple(args) if key_fn is None else key_fn(*args)
+            is_cached, value = cache.get(key)
+            if not is_cached:
+                value = fn(*args, **kwargs)
+                cache.put(key, value)
+            return value
+        return lookup_or_execute
+    return lru_decorator
---
+++ tests/test_caching.py
@@ -0,0 +1,83 @@
+from AlphaGo.preprocessing.caching import LRUCache, lru_cache
+import unittest
+
+
+class TestLRUCache(unittest.TestCase):
+
+    def setUp(self):
+        self.cache = LRUCache(max_size=3)
+
+    def testGetPutDelete(self):
+        self.assertEqual(self.cache.size, 0)
+        self.cache.put('a', 1)
+        self.cache.put('b', 2)
+        self.cache.put('c', 3)
+        self.assertEqual(self.cache.size, 3)
+
+        # 'c' should be cached and most-recently-used.
+        # Current cache order: a b c
+        is_cached, value = self.cache.get('c')
+        self.assertTrue(is_cached)
+        self.assertEqual(value, 3)
+
+        # A 4th insertion should evict 'a', which is least-recently used.
+        # Current cache order: a b c
+        self.cache.put('d', 4)
+        # Current cache order: b c d
+        self.assertEqual(self.cache.size, 3)
+        is_cached, value = self.cache.get('a')
+        self.assertFalse(is_cached)
+        self.assertIsNone(value)
+        self.assertEqual(self.cache.size, 3)
+
+        # 'b' is LRU. After querying it, 'c' should be LRU.
+        # Current cache order: b c d
+        self.cache.get('b')
+        # Current cache order: c d b
+        self.cache.put('e', 5)
+        # Current cache order: d b e
+        is_cached, value = self.cache.get('c')
+        self.assertFalse(is_cached)
+        self.assertEqual(self.cache.size, 3)
+
+        # test deletion.
+        self.cache.delete('d')
+        # Current cache order: b e
+        self.assertEqual(self.cache.size, 2)
+        is_cached, value = self.cache.get('d')
+        self.assertFalse(is_cached)
+
+
+class TestDecorator(unittest.TestCase):
+
+    def testNoKeyFn(self):
+        @lru_cache(max_size=3)
+        def fn(x, y=0):
+            return x + y
+        self.assertEqual(fn(1), 1)
+        self.assertEqual(fn(2), 2)
+        # fn should return the cached value here
+        self.assertEqual(fn(1, y=100), 1)
+        self.assertEqual(fn(2), 2)
+        self.assertEqual(fn(3), 3)
+        self.assertEqual(fn(4), 4)
+        # key '1' is expired. Value will be recomputed.
+        self.assertEqual(fn(1, y=100), 101)
+
+    def testKeyFn(self):
+        # Demonstrate key functions using absolute value of input as key.
+        @lru_cache(max_size=3, key_fn=abs)
+        def fn(x):
+            return x + 1
+        self.assertEqual(fn(1), 2)
+        self.assertEqual(fn(2), 3)
+        # fn should return the cached value here
+        self.assertEqual(fn(-1), 2)
+        self.assertEqual(fn(2), 3)
+        self.assertEqual(fn(3), 4)
+        self.assertEqual(fn(4), 5)
+        # key '1' is expired. Value will be recomputed.
+        self.assertEqual(fn(-1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
