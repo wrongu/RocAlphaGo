c59d151c68c5f6cb238b1cb044cf07994ec8520a
MaMiFreak <ichbunut@gmail.com>
2016-12-22 10:14:12 +0100

    update SL tests

--- tests/test_supervised_policy_trainer.py
+++ tests/test_supervised_policy_trainer.py
@@ -1,6 +1,8 @@
 import os
-from AlphaGo.training.supervised_policy_trainer import run_training
+from AlphaGo.training.policy.supervised import run_training
+from AlphaGo.training.policy.supervised import load_indices_from_file
 import unittest
+import numpy as np
 
 
 class TestSupervisedPolicyTrainer(unittest.TestCase):
@@ -8,12 +10,79 @@ class TestSupervisedPolicyTrainer(unittest.TestCase):
         model = 'tests/test_data/minimodel.json'
         data = 'tests/test_data/hdf5/alphago-vs-lee-sedol-features.hdf5'
         output = 'tests/test_data/.tmp.training/'
-        args = [model, data, output, '--epochs', '1']
+        args = [model, data, output, '--epochs', '1', '-l', '160']
         run_training(args)
 
+        # remove temporary files
         os.remove(os.path.join(output, 'metadata.json'))
-        os.remove(os.path.join(output, 'shuffle.npz'))
+        os.remove(os.path.join(output, 'shuffle_train.npz'))
+        os.remove(os.path.join(output, 'shuffle_validate.npz'))
+        os.remove(os.path.join(output, 'shuffle_test.npz'))
         os.remove(os.path.join(output, 'weights.00000.hdf5'))
+        os.rmdir(output)
+
+    def testResumeLearning(self):
+        model = 'tests/test_data/minimodel.json'
+        data = 'tests/test_data/hdf5/alphago-vs-lee-sedol-features.hdf5'
+        output = 'tests/test_data/.tmp.resume-training/'
+        args = [model, data, output, '--epochs', '1', '-l', '160']
+        run_training(args)
+
+        # resume learning
+        args = [model, data, output, '--epochs', '1', '-l', '160',
+                '--weights', 'weights.00000.hdf5']
+        run_training(args)
+
+        # remove temporary files
+        os.remove(os.path.join(output, 'metadata.json'))
+        os.remove(os.path.join(output, 'shuffle_train.npz'))
+        os.remove(os.path.join(output, 'shuffle_validate.npz'))
+        os.remove(os.path.join(output, 'shuffle_test.npz'))
+        os.remove(os.path.join(output, 'weights.00000.hdf5'))
+        os.remove(os.path.join(output, 'weights.00001.hdf5'))
+        os.rmdir(output)
+
+    def testStateAmount(self):
+        model = 'tests/test_data/minimodel.json'
+        data = 'tests/test_data/hdf5/alphago-vs-lee-sedol-features.hdf5'
+        output = 'tests/test_data/.tmp.state-amount/'
+        args = [model, data, output, '--epochs', '0', '--train-val-test', '.9', '.05', '.05']
+        run_training(args)
+
+        # shuffle file locations for train/validation/test set
+        shuffle_file_train = os.path.join(output, "shuffle_train.npz")
+        shuffle_file_val = os.path.join(output, "shuffle_validate.npz")
+        shuffle_file_test = os.path.join(output, "shuffle_test.npz")
+
+        # load from .npz files
+        # load training set
+        train_indices = load_indices_from_file(shuffle_file_train)
+        # count unique rows
+        unique_indices = len(np.vstack({tuple(row) for row in train_indices}))
+        self.assertTrue(unique_indices == 7438)
+
+        # load validation set
+        val_indices = load_indices_from_file(shuffle_file_val)
+        # count unique rows
+        unique_indices = len(np.vstack({tuple(row) for row in val_indices}))
+        self.assertTrue(unique_indices == 413)
+
+        # load training set
+        test_indices = load_indices_from_file(shuffle_file_test)
+        # count unique rows
+        unique_indices = len(np.vstack({tuple(row) for row in test_indices}))
+        self.assertTrue(unique_indices == 413)
+
+        # combine all set rows
+        combined_indices = np.concatenate((train_indices, val_indices, test_indices), axis=0)
+        # count unique rows
+        unique_indices = len(np.vstack({tuple(row) for row in combined_indices}))
+        self.assertTrue(unique_indices == (7438 + 413 + 413))
+
+        # remove temporary files
+        os.remove(shuffle_file_train)
+        os.remove(shuffle_file_val)
+        os.remove(shuffle_file_test)
         os.rmdir(output)
 
 
