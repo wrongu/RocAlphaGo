132294615ff93bf3eb092a2dca25de678cf5cc62
MaMiFreak <ichbunut@gmail.com>
2016-12-27 18:09:41 +0100

    added confirm prompt function

--- AlphaGo/util.py
+++ AlphaGo/util.py
@@ -8,6 +8,36 @@ from AlphaGo import go
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 
+def confirm(prompt=None, resp=False):
+    """prompts for yes or no response from the user. Returns True for yes and
+       False for no.
+       'resp' should be set to the default value assumed by the caller when
+       user simply types ENTER.
+       created by:
+       http://code.activestate.com/recipes/541096-prompt-the-user-for-confirmation/
+    """
+
+    if prompt is None:
+        prompt = 'Confirm'
+
+    if resp:
+        prompt = '%s [%s]|%s: ' % (prompt, 'y', 'n')
+    else:
+        prompt = '%s [%s]|%s: ' % (prompt, 'n', 'y')
+
+    while True:
+        ans = raw_input(prompt)
+        if not ans:
+            return resp
+        if ans not in ['y', 'Y', 'n', 'N']:
+            print 'please enter y or n.'
+            continue
+        if ans == 'y' or ans == 'Y':
+            return True
+        if ans == 'n' or ans == 'N':
+            return False
+
+
 def flatten_idx(position, size):
     (x, y) = position
     return x * size + y
