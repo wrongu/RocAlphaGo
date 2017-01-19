d1401b4bb83d61c27a7978653eac232f0faa5063
MaMiFreak <ichbunut@gmail.com>
2017-01-19 22:33:05 +0100

    greedy_start option for ProbabilisticPolicyPlayer; fixed apply_temperature bug

--- AlphaGo/ai.py
+++ AlphaGo/ai.py
@@ -1,13 +1,13 @@
 """Policy players"""
 import numpy as np
-from operator import itemgetter
 from AlphaGo import go
 from AlphaGo import mcts
+from operator import itemgetter
 
 
 class GreedyPolicyPlayer(object):
     """A player that uses a greedy policy (i.e. chooses the highest probability
-    move each turn)
+       move each turn)
     """
 
     def __init__(self, policy_function, pass_when_offered=False, move_limit=None):
@@ -16,35 +16,43 @@ class GreedyPolicyPlayer(object):
         self.move_limit = move_limit
 
     def get_move(self, state):
+        # check move limit
         if self.move_limit is not None and len(state.history) > self.move_limit:
             return go.PASS_MOVE
+
+        # check if pass was offered and we want to pass
         if self.pass_when_offered:
             if len(state.history) > 100 and state.history[-1] == go.PASS_MOVE:
                 return go.PASS_MOVE
+
+        # list with sensible moves
         sensible_moves = [move for move in state.get_legal_moves(include_eyes=False)]
+
+        # check if there are sensible moves left to do
         if len(sensible_moves) > 0:
             move_probs = self.policy.eval_state(state, sensible_moves)
             max_prob = max(move_probs, key=itemgetter(1))
             return max_prob[0]
+
         # No 'sensible' moves available, so do pass move
         return go.PASS_MOVE
 
 
 class ProbabilisticPolicyPlayer(object):
     """A player that samples a move in proportion to the probability given by the
-    policy.
-
-    By manipulating the 'temperature', moves can be pushed towards totally random
-    (high temperature) or towards greedy play (low temperature)
+       policy.
+       By manipulating the 'temperature', moves can be pushed towards totally random
+       (high temperature) or towards greedy play (low temperature)
     """
 
-    def __init__(self, policy_function, temperature=1.0, pass_when_offered=False, move_limit=None):
+    def __init__(self, policy_function, temperature=1.0, pass_when_offered=False,
+                 move_limit=None, greedy_start=None):
         assert(temperature > 0.0)
         self.policy = policy_function
         self.move_limit = move_limit
         self.beta = 1.0 / temperature
         self.pass_when_offered = pass_when_offered
-        self.move_limit = move_limit
+        self.greedy_start = greedy_start
 
     def apply_temperature(self, distribution):
         log_probabilities = np.log(distribution)
@@ -58,23 +66,42 @@ class ProbabilisticPolicyPlayer(object):
         return probabilities / probabilities.sum()
 
     def get_move(self, state):
+        # check move limit
         if self.move_limit is not None and len(state.history) > self.move_limit:
             return go.PASS_MOVE
+
+        # check if pass was offered and we want to pass
         if self.pass_when_offered:
             if len(state.history) > 100 and state.history[-1] == go.PASS_MOVE:
                 return go.PASS_MOVE
+
+        # list with 'sensible' moves
         sensible_moves = [move for move in state.get_legal_moves(include_eyes=False)]
+
+        # check if there are 'sensible' moves left to do
         if len(sensible_moves) > 0:
+
             move_probs = self.policy.eval_state(state, sensible_moves)
-            # zip(*list) is like the 'transpose' of zip;
-            # zip(*zip([1,2,3], [4,5,6])) is [(1,2,3), (4,5,6)]
-            moves, probabilities = zip(*move_probs)
-            # apply 'temperature' to the distribution
-            probabilities = self.apply_temperature(probabilities)
-            # numpy interprets a list of tuples as 2D, so we must choose an
-            # _index_ of moves then apply it in 2 steps
-            choice_idx = np.random.choice(len(moves), p=probabilities)
-            return moves[choice_idx]
+
+            if self.greedy_start is not None and len(state.history) >= self.greedy_start:
+                # greedy
+
+                max_prob = max(move_probs, key=itemgetter(1))
+                return max_prob[0]
+            else:
+                # probabilistic
+
+                # zip(*list) is like the 'transpose' of zip;
+                # zip(*zip([1,2,3], [4,5,6])) is [(1,2,3), (4,5,6)]
+                moves, probabilities = zip(*move_probs)
+                # apply 'temperature' to the distribution
+                probabilities = self.apply_temperature(probabilities)
+                # numpy interprets a list of tuples as 2D, so we must choose an
+                # _index_ of moves then apply it in 2 steps
+                choice_idx = np.random.choice(len(moves), p=probabilities)
+                return moves[choice_idx]
+
+        # No 'sensible' moves available, so do pass move
         return go.PASS_MOVE
 
     def get_moves(self, states):
@@ -88,13 +115,21 @@ class ProbabilisticPolicyPlayer(object):
             if len(move_probs) == 0 or len(states[i].history) > self.move_limit:
                 move_list[i] = go.PASS_MOVE
             else:
-                # this 'else' clause is identical to ProbabilisticPolicyPlayer.get_move
-                moves, probabilities = zip(*move_probs)
-                probabilities = np.array(probabilities)
-                probabilities = probabilities ** self.beta
-                probabilities = probabilities / probabilities.sum()
-                choice_idx = np.random.choice(len(moves), p=probabilities)
-                move_list[i] = moves[choice_idx]
+                if self.greedy_start is not None and len(states[i].history) >= self.greedy_start:
+                    # greedy
+
+                    max_prob = max(move_probs, key=itemgetter(1))
+                    move_list[i] = max_prob[0]
+                else:
+                    # probabilistic
+
+                    moves, probabilities = zip(*move_probs)
+                    # apply 'temperature' to the distribution
+                    probabilities = self.apply_temperature(probabilities)
+                    # numpy interprets a list of tuples as 2D, so we must choose an
+                    # _index_ of moves then apply it in 2 steps
+                    choice_idx = np.random.choice(len(moves), p=probabilities)
+                    move_list[i] = moves[choice_idx]
         return move_list
 
 
