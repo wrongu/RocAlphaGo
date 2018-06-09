04bac9c9d16c16c3baf16988ac761903a687d4de
wrongu <rlange@ur.rochester.edu>
2018-06-09 09:54:42 -0400

    Fixed Travis-CI Theano build, updated requirements

--- setup.py
+++ setup.py
@@ -5,19 +5,26 @@ from Cython.Build import cythonize
 
 extensions = [
     Extension("AlphaGo.go.constants", ["AlphaGo/go/constants.pyx"],
-              include_dirs=[numpy.get_include()], language="c++"),
+              include_dirs=[numpy.get_include()], language="c++",
+              extra_compile_args=["-std=c++11"], extra_link_args=["-std=c++11"]),
     Extension("AlphaGo.go.ladders", ["AlphaGo/go/ladders.pyx"],
-              include_dirs=[numpy.get_include()], language="c++"),
+              include_dirs=[numpy.get_include()], language="c++",
+              extra_compile_args=["-std=c++11"], extra_link_args=["-std=c++11"]),
     Extension("AlphaGo.go.game_state", ["AlphaGo/go/game_state.pyx"],
-              include_dirs=[numpy.get_include()], language="c++"),
+              include_dirs=[numpy.get_include()], language="c++",
+              extra_compile_args=["-std=c++11"], extra_link_args=["-std=c++11"]),
     Extension("AlphaGo.go.group_logic", ["AlphaGo/go/group_logic.pyx"],
-              include_dirs=[numpy.get_include()], language="c++"),
+              include_dirs=[numpy.get_include()], language="c++",
+              extra_compile_args=["-std=c++11"], extra_link_args=["-std=c++11"]),
     Extension("AlphaGo.go.coordinates", ["AlphaGo/go/coordinates.pyx"],
-              include_dirs=[numpy.get_include()], language="c++"),
+              include_dirs=[numpy.get_include()], language="c++",
+              extra_compile_args=["-std=c++11"], extra_link_args=["-std=c++11"]),
     Extension("AlphaGo.go.zobrist", ["AlphaGo/go/zobrist.pyx"],
-              include_dirs=[numpy.get_include()], language="c++"),
+              include_dirs=[numpy.get_include()], language="c++",
+              extra_compile_args=["-std=c++11"], extra_link_args=["-std=c++11"]),
     Extension("AlphaGo.preprocessing.preprocessing", ["AlphaGo/preprocessing/preprocessing.pyx"],
-              include_dirs=[numpy.get_include()], language="c++"),
+              include_dirs=[numpy.get_include()], language="c++",
+              extra_compile_args=["-std=c++11"], extra_link_args=["-std=c++11"]),
 ]
 
 setup(name="RocAlphaGo", ext_modules=cythonize(extensions))
